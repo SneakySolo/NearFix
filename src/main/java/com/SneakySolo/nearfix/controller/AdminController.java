e73b24756d83c7ba5529bb136dfceafc81996de4
SneakySolo <adiofficial15@gmail.com>
2026-04-15 22:42:29 +0530

    Admin third commit

--- src/main/java/com/SneakySolo/nearfix/controller/AdminController.java
+++ src/main/java/com/SneakySolo/nearfix/controller/AdminController.java
@@ -1,15 +1,19 @@
 package com.SneakySolo.nearfix.controller;
 
+import com.SneakySolo.nearfix.domain.message.Message;
 import com.SneakySolo.nearfix.domain.user.Role;
 import com.SneakySolo.nearfix.domain.user.User;
+import com.SneakySolo.nearfix.entity.AdminMessage;
+import com.SneakySolo.nearfix.repository.AdminMessageRepository;
+import com.SneakySolo.nearfix.repository.UserRepository;
+import com.SneakySolo.nearfix.service.AdminMessageService;
+import com.SneakySolo.nearfix.service.SessionService;
 import com.SneakySolo.nearfix.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +23,9 @@ import java.util.List;
 public class AdminController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
+    private final AdminMessageService adminMessageService;
+    private final SessionService sessionService;
 
     @GetMapping("/dashboard")
     public String getCustomerAndProvider (Model model){
@@ -31,9 +38,48 @@ public class AdminController {
         return "admin/dashboard";
     }
 
-    @PostMapping("/admin/user/{userId}/toggle")
+    @PostMapping("/user/{userId}/toggle")
     public String toggleUserStatus(@PathVariable Integer id) {
         userService.toggleUserStatus(id);
         return "/dashboard";
+    }
+
+    @GetMapping("/chat/{targetUserId}")
+    public String getMessages (@PathVariable Integer id,
+                               HttpSession session, Model model) {
+        List<AdminMessage> messages = adminMessageService.getMessages(id);
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("error"));
+
+        model.addAttribute("user", user);
+        model.addAttribute("userId", user.getId());
+        model.addAttribute("messages", messages);
+        model.addAttribute("adminId",sessionService.getUserId(session));
+
+        return "admin/chat";
+    }
+
+    @PostMapping("/chat/{targetUserId}/send")
+    public String sendMessages (@PathVariable Integer id,
+                                @RequestParam String message,
+                                HttpSession session) {
+        adminMessageService.sendMessage(id,
+                sessionService.getUserId(session),
+                id, message);
+
+        return "redirect:/admin/chat/{targetUserId}" + id;
+    }
+
+    @GetMapping("/admin/chat/{targetUserId}/messages")
+    public String getMessageFragments (@PathVariable Integer requestId,
+                                       HttpSession session, Model model) {
+
+        List<AdminMessage> messages = adminMessageService.getMessages(requestId);
+        Integer userId = sessionService.getUserId(session);
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("userId", userId);
+
+        return "/chat-fragment";
     }
 }
